2746f6b73a6e730215cf1d8a1d36e51457e2fd00
Istem Karakas <istem.karakas@gmail.com>
2025-04-24 14:31:27 +0100

    Implement user authentication context and update NavBar UI based on login state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext)
+  const loggedInIcons = <>{currentUser?.username}</>
+  const loggedOutIcons = (
+   <>
+    <NavLink 
+      className={styles.NavLink} 
+      activeClassName={styles.Active} 
+      to="/signin">
+      <i className="fas fa-sign-in-alt"></i>Sign in
+    </NavLink>
+    <NavLink 
+      className={styles.NavLink} 
+      activeClassName={styles.Active}
+      to="/signup">
+      <i className="fas fa-user-plus"></i>Sign up
+    </NavLink>
+   </>
+  );
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
@@ -22,18 +42,7 @@ const NavBar = () => {
             to="/">
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink 
-            className={styles.NavLink} 
-            activeClassName={styles.Active} 
-            to="/signin">
-              <i className="fas fa-sign-in-alt"></i>Sign in
-            </NavLink>
-            <NavLink 
-            className={styles.NavLink} 
-            activeClassName={styles.Active}
-            to="/signup">
-              <i className="fas fa-user-plus"></i>Sign up
-            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
