d524ea112cc061f7e683a375dfe17d8a393dc86a
Istem Karakas <istem.karakas@gmail.com>
2025-04-28 14:35:25 +0100

    Fetch and display single post data on PostPage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import bgImage from "./assets/bgimage.jpg";
+import PostPage from "./pages/posts/PostPage";
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
             <Route exact path="/signin" render={() => <SignInForm />} />
             <Route exact path="/signup" render={() => <SignUpForm /> } />
             <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+            <Route exact path="/posts/:id" render={()=> <PostPage />} />
             <Route render={()=><p>Page not found!</p>} />
           </Switch>
         </Container>
