2385610703bdac24159c87ece9880d0082694f15
Istem Karakas <istem.karakas@gmail.com>
2025-05-27 01:20:20 +0100

    Updated styling for the Most Followed Profiles section on the /feed page

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import appStyles from "../../App.module.css";
+import appStyles from "../../styles/App.module.css";
 import Asset from "../../components/Asset";
 import { useProfileData } from "../../contexts/ProfileDataContext";
 import Profile from "./Profile";
 
+console.log("appStyles:", appStyles);
+
+
 const PopularProfiles = ({ mobile }) => {
   const { popularProfiles } = useProfileData();
-
+  
   return (
     <Container
-      className={`${appStyles.Content} ${
-        mobile && "d-lg-none text-center mb-3"
-      }`}
+  className={`${appStyles.Content} ${appStyles.popularProfilesContainer} ${
+    mobile && "d-lg-none text-center mb-3"
+  }`}
     >
       {popularProfiles.results.length ? (
-        <>
-          <p>Most followed profiles.</p>
+        <div className={appStyles.popularProfilesBox}>
+          <p className={appStyles.popularProfilesTitle}>
+            Most followed profiles.</p>
           {mobile ? (
             <div className="d-flex justify-content-around">
               {popularProfiles.results.slice(0, 4).map((profile) => (
@@ -28,11 +32,12 @@ const PopularProfiles = ({ mobile }) => {
               <Profile key={profile.id} profile={profile} />
             ))
           )}
-        </>
+        </div>
       ) : (
         <Asset spinner />
       )}
     </Container>
+    
   );
 };
 
