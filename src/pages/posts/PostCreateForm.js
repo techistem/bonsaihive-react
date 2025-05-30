b3786a354e4ecd17110cd017babb515774fd46a6
Istem Karakas <istem.karakas@gmail.com>
2025-05-30 15:23:39 +0100

    .ImagePreview

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -13,7 +13,6 @@ import Asset from "../../components/Asset";
 import Upload from "../../assets/upload.png";
 
 import styles from "../../styles/PostCreateEditForm.module.css";
-import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
 import { useHistory } from "react-router";
@@ -73,7 +72,8 @@ function PostCreateForm() {
   const textFields = (
     <div className="text-center">
       <Form.Group>
-        <Form.Label>Title</Form.Label>
+      <Form.Label className={styles.CustomLabel}>Title</Form.Label>
+
         <Form.Control
           type="text"
           name="title"
@@ -88,7 +88,7 @@ function PostCreateForm() {
       ))}
 
       <Form.Group>
-        <Form.Label>Content</Form.Label>
+      <Form.Label className={styles.CustomLabel}>Content</Form.Label>
         <Form.Control
           as="textarea"
           rows={6}
@@ -116,61 +116,61 @@ function PostCreateForm() {
   );
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Row>
-        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
-          <Container
-            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
-          >
-            <Form.Group className="text-center">
-              {image ? (
-                <>
-                  <figure>
-                    <Image className={appStyles.Image} src={image} rounded />
-                  </figure>
-                  <div>
+      <div className={styles.FormWrapper}>
+        <Form onSubmit={handleSubmit}>
+          <Row>
+            <Col className="py-2 p-0 p-md-2" md={12}>
+              <Container className="d-flex flex-column justify-content-center">
+                <Form.Group className="text-center">
+                  {image ? (
+                    <>
+                      <figure>
+                      <Image className={styles.ImagePreview} src={image} rounded />
+                      </figure>
+                      <div>
+                        <Form.Label
+                          className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                          htmlFor="image-upload"
+                        >
+                          Change the image
+                        </Form.Label>
+                      </div>
+                    </>
+                  ) : (
                     <Form.Label
-                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                      className="d-flex justify-content-center"
                       htmlFor="image-upload"
+                      style={{ color: "#385212", fontWeight: "700" }}
                     >
-                      Change the image
+                      <Asset
+                        src={Upload}
+                        message="Click or tap to upload an image"
+
+                      />
                     </Form.Label>
-                  </div>
-                </>
-              ) : (
-                <Form.Label
-                  className="d-flex justify-content-center"
-                  htmlFor="image-upload"
-                >
-                  <Asset
-                    src={Upload}
-                    message="Click or tap to upload an image"
+                  )}
+  
+                  <Form.File
+                    id="image-upload"
+                    accept="image/*"
+                    onChange={handleChangeImage}
+                    ref={imageInput}
                   />
-                </Form.Label>
-              )}
-
-              <Form.File
-                id="image-upload"
-                accept="image/*"
-                onChange={handleChangeImage}
-                ref={imageInput}
-              />
-            </Form.Group>
-            {errors?.image?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-
-            <div className="d-md-none">{textFields}</div>
-          </Container>
-        </Col>
-        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
-          <Container className={appStyles.Content}>{textFields}</Container>
-        </Col>
-      </Row>
-    </Form>
+                </Form.Group>
+                {errors?.image?.map((message, idx) => (
+                  <Alert variant="warning" key={idx}>
+                    {message}
+                  </Alert>
+                ))}
+  
+                {textFields}
+              </Container>
+            </Col>
+          </Row>
+        </Form>
+      </div>
   );
+  
 }
 
 export default PostCreateForm;
