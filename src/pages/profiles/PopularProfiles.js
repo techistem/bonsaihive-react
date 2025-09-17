48baf9d2c4db02ae3db581e47c282ad218d2abeb
Istem Karakas <istem.karakas@gmail.com>
2025-09-17 20:17:16 +0100

    fix: improve formatting and readability in PopularProfiles component

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -7,20 +7,20 @@ import Profile from "./Profile";
 
 console.log("appStyles:", appStyles);
 
-
 const PopularProfiles = ({ mobile }) => {
   const { popularProfiles } = useProfileData();
-  
+
   return (
     <Container
-  className={`${appStyles.Content} ${appStyles.popularProfilesContainer} ${
-    mobile && "d-lg-none text-center mb-3"
-  }`}
+      className={`${appStyles.Content} ${appStyles.popularProfilesContainer} ${
+        mobile && "d-lg-none text-center mb-3"
+      }`}
     >
       {popularProfiles.results.length ? (
         <div className={appStyles.popularProfilesBox}>
           <p className={appStyles.popularProfilesTitle}>
-            Most followed profiles.</p>
+            Most followed profiles
+          </p>
           {mobile ? (
             <div className="d-flex justify-content-around">
               {popularProfiles.results.slice(0, 4).map((profile) => (
@@ -37,7 +37,6 @@ const PopularProfiles = ({ mobile }) => {
         <Asset spinner />
       )}
     </Container>
-    
   );
 };
 
