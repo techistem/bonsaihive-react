95a0bcaf6e9a3233d40cb7c78e37ff4a983f8f44
Istem Karakas <istem.karakas@gmail.com>
2025-09-26 12:08:28 +0100

    refactor: improve layout and accessibility in Profile component

--- TESTING.md
+++ TESTING.md
@@ -12,3 +12,16 @@
 All files has been checked and gave no errors.
 
 ![CSS](docs/readme-images/CSS_validation.png)
+
+## ESlint
+
+[ESlint](https://eslint.org/) - Has been used during developement and had no errors.
+
+![ESlint](docs/readme-images/ESlint.png)
+
+## Lighthouse
+
+![Lighthouse](docs/readme-images/lighthouse.png)
+
+Lighthouse testing has been performed on all pages. No significants dropdowns other than the listings page
+performance that contain large images (user's uploads).
--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -8,45 +8,51 @@ import { Button } from "react-bootstrap";
 import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
 const Profile = (props) => {
-    const { profile, mobile, imageSize = 55 } = props;
-    const { id, following_id, image, owner } = profile;
+  const { profile, mobile, imageSize = 55 } = props;
+  const { id, following_id, image, owner } = profile;
 
-    const currentUser = useCurrentUser();
-    const is_owner = currentUser?.username === owner;
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
-    const { handleFollow, handleUnfollow } = useSetProfileData();
+  const { handleFollow, handleUnfollow } = useSetProfileData();
 
   return (
     <div
       className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
     >
-       <div>
+      <div>
         <Link className="align-self-center" to={`/profiles/${id}`}>
-           <Avatar src={image} height={imageSize} />
+          <Avatar
+            src={image}
+            alt={`${owner}'s profile picture`}
+            height={imageSize}
+          />
         </Link>
-    </div>
-    <div className={`mx-2 ${styles.Wordbreak}`}>
+      </div>
+      <div className={`mx-2 ${styles.Wordbreak}`}>
         <strong>{owner}</strong>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+              onClick={() => handleUnfollow(profile)}
+            >
+              unfollow
+            </Button>
+          ) : (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              onClick={() => handleFollow(profile)}
+            >
+              follow
+            </Button>
+          ))}
+      </div>
     </div>
-    <div className={`text-right ${!mobile && 'ml-auto'}`}>
-        {!mobile && currentUser && !is_owner && (
-            following_id ? (
-                <Button
-                className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
-                onClick={() => handleUnfollow(profile)}
-              >
-                unfollow
-                </Button>
-            ) : (
-                <Button
-                className={`${btnStyles.Button} ${btnStyles.Black}`}
-                onClick={() => handleFollow(profile)}
-              >
-                follow
-                </Button>
-            ))}
-    </div>
-   </div>
   );
 };
 
